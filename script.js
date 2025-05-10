c0316da14719de12bbc4cbe0560bf97eb639b6a6
Amir <amir.omar.hatim@gmail.com>
2025-05-10 16:37:36 +0300

    Updated book layout and styles

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function filterByGenre(genre) {
     alert(`Фильтрация по жанру: ${genre} (в разработке)`);
   }
+  function filterBooks(category) {
+    const books = document.querySelectorAll('.book-card');
+  
+    books.forEach(book => {
+      if (category === 'all') {
+        book.style.display = 'block';
+      } else {
+        if (book.classList.contains(category)) {
+          book.style.display = 'block';
+        } else {
+          book.style.display = 'none';
+        }
+      }
+    });
+  }
